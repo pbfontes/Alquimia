f49bab92e044fb94cec375dbabfefeb9011c985d
pbfontes <paulobatistaft@gmail.com>
2021-02-24 22:06:37 -0300

    Data base da tabela periodica criada

--- alquimia/estequiometria/admin.py
+++ alquimia/estequiometria/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Pesquisa
+from .models import Pesquisa, TabelaPeriodica
 
 admin.site.register(Pesquisa)
+admin.site.register(TabelaPeriodica)
